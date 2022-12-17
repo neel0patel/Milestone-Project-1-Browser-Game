60cad1bd3aaaa8bb0185216c3d2cf7a9b5a6c28e
Neel Patel <neelpatel@Neels-MacBook-Pro.local>
2022-12-17 18:23:07 -0500

    changed CSS to SCSS

--- game.js
+++ game.js
@@ -9,6 +9,7 @@ var button = document.createElement("button");
 var characterTop;
 var characterLeft;
 
+// starts the audio as soon as the start game button is clicked
 var audio = document.getElementById("audio");
 var button = document.getElementById("button");
 
@@ -29,6 +30,7 @@ button.addEventListener("click", function () {
 });
 //start game function
 function startGame() {
+  // function moving the character left and right
   function moveLeft() {
     let characterXpos = parseInt(
       window.getComputedStyle(character).getPropertyValue("left")
@@ -46,7 +48,7 @@ function startGame() {
       character.style.left = characterXpos + 2 + "px";
     }
   }
-
+  // allows the ball to move left and right based on what keys are pressed
   document.addEventListener("keydown", function (e) {
     if (both == 0) {
       both++;
@@ -58,10 +60,12 @@ function startGame() {
       }
     }
   });
+  // clear the interval so animation stops
   document.addEventListener("keyup", function (e) {
     clearInterval(charMove);
     both = 0;
   });
+  // creating the blocks and holes of the game and randomly generating a new hole with a new line of block
   var blocks = setInterval(function () {
     blockTop = 0;
     holeTop = 0;
@@ -92,7 +96,7 @@ function startGame() {
       counterArray.push(counter);
       counter++;
     }
-
+    // if the ball is above the hole in block, it should drop
     characterTop = parseInt(
       window.getComputedStyle(character).getPropertyValue("top")
     );
@@ -107,7 +111,7 @@ function startGame() {
       clearInterval(blocks);
       location.reload();
     }
-
+    // this section of code makes the blocks move upward as the ball continues to drop down
     for (i = 0; i < counterArray.length; i++) {
       relative = counterArray[i];
       rBlock = document.getElementById("block" + relative);
@@ -127,7 +131,7 @@ function startGame() {
         rBlock.remove();
         rHole.remove();
       }
-      // if ball
+      // if the block and ball are touching surfaces, we make the drop a plus so (no drop)
       if (relativeBlock - 20 < characterTop && relativeBlock > characterTop) {
         drop++;
         // if current hole and ball are in the same position, drop ball
@@ -139,11 +143,12 @@ function startGame() {
         }
       }
     }
-
+    // if the drop is 0 then the character moves to the top
     if (drop == 0) {
       if (characterTop < 400) {
         character.style.top = characterTop + 2 + "px";
       }
+      // if drop isnt 0 and it's incrementing, the character should go with the block
     } else {
       character.style.top = characterTop - 2 + "px";
     }
